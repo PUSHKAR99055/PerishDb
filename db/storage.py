623a0b16166f620ff8489bd7f3a047c525a9d969
Your Name <99055pushkar@gmail.com>
2024-10-05 18:41:49 -0400

    changes

--- db/storage.py
+++ db/storage.py
@@ -1,5 +1,8 @@
 import portalocker
 class Storage(object):
+	def __init__(self, file):
+		self._f = file  # This should accept the file object
+		self.locked = False
 	def lock(self):
 		if not self.locked:
 			portalocker.lock(self._f, portalocker.LOCK_EX)
--- db/tool.py
+++ db/tool.py
@@ -1,50 +1,45 @@
+from __future__ import print_function
 import sys
-import getopt
-from . import connect
-#from PERISHDB import DBDB as PerishDb
-from . import perish_db
+
+import db
+
+
+OK = 0
+BAD_ARGS = 1
+BAD_VERB = 2
+BAD_KEY = 3
+
 
 def usage():
-    print("Usage: python tool.py <dbname> <verb> <key> [value]")
-    print("  <dbname>: The database file to use")
-    print("  <verb>: 'get', 'set', or 'delete'")
-    print("    'get' requires a <key>")
-    print("    'set' requires a <key> and <value>")
-    print("    'delete' requires a <key>")
-    print("\nExample:")
-    print("  python tool.py mydb.db set mykey myvalue")
-    print("  python tool.py mydb.db get mykey")
-    print("  python tool.py mydb.db delete mykey")
-
-OK = 0         # Success
-BAD_ARGS = 1   # Incorrect number of arguments
-BAD_VERB = 2   # Invalid verb (not 'get', 'set', 'delete')
-BAD_KEY = 3    # Key not found
+    print("Usage:", file=sys.stderr)
+    print("\tpython -m dbdb.tool DBNAME get KEY", file=sys.stderr)
+    print("\tpython -m dbdb.tool DBNAME set KEY VALUE", file=sys.stderr)
+    print("\tpython -m dbdb.tool DBNAME delete KEY", file=sys.stderr)
 
 
 def main(argv):
-	print(argv[1])
-	if not (4 <= len(argv) <= 5):
-		usage() #define usage
-		return BAD_ARGS
-	dbname, verb, key, value = (argv[1:] + [None])[:4]
-	print(dbname, verb, key)
-	if verb not in {'get', 'set', 'delete'}:
-		usage()
-		return BAD_VERB
-	db = connect(dbname)
-	try:
-		if verb == 'get':
-			sys.stdout.write(db[key])
-		elif verb == 'set':
-			db[key] = value
-			db.commit()
-		else:
-			del db[key]
-			db.commit()
-	except KeyError:
-		print("Key not found", file = sys.stderr)
-		return BAD_KEY
-	return OK
-if __name__ == "__main__":
-	main(sys.argv)
+    if not (4 <= len(argv) <= 5):
+        usage()
+        return BAD_ARGS
+    dbname, verb, key, value = (argv[1:] + [None])[:4]
+    if verb not in {'get', 'set', 'delete'}:
+        usage()
+        return BAD_VERB
+    db = db.connect(dbname)
+    try:
+        if verb == 'get':
+            sys.stdout.write(db[key])
+        elif verb == 'set':
+            db[key] = value
+            db.commit()
+        else:
+            del db[key]
+            db.commit()
+    except KeyError:
+        print("Key not found", file=sys.stderr)
+        return BAD_KEY
+    return OK
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
